3dcfcda8392195e641c153af65ff0896bde45fe5
Ulug'bek Xudoyberdiyev <ulugbek.khudoyberdiyev@gmail.com>
2025-06-11 15:59:17 +0500

    Update lesson9.sql

--- lesson-9/homework/lesson9.sql
+++ lesson-9/homework/lesson9.sql
@@ -45,3 +45,84 @@ Select*from Orders as A
 join Products as B
 on A.ProductID=B.ProductID
 where Price>100
+
+--11
+Select A.Name as Emp_Name, B.DepartmentName from Employees as A
+cross join Departments as B
+where A.DepartmentID<>B.DepartmentID;
+
+--12
+Select O.OrderID,P.ProductName,O.Quantity,P.StockQuantity from Orders O
+join Products P
+on O.ProductID=P.ProductID
+where O.Quantity>P.StockQuantity
+
+--13
+Select C.FirstName,C.LastName,S.ProductID,S.SaleAmount from Customers1 C
+Join Sales S
+on C.CustomerID=S.CustomerID
+where S.SaleAmount>=500
+
+--14
+Select S.Name, C.CourseName from Courses C
+join Enrollments E
+on C.CourseID=E.CourseID
+join Students S
+on E.StudentID=S.StudentID
+
+--15
+Select P.ProductName,S.SupplierName from Products P
+Join Suppliers S
+on P.SupplierID=S.SupplierID
+where S.SupplierName like '%Tech%'
+
+--16
+Select O.OrderID, O.TotalAmount, P.Amount from Orders O
+Join Payments P
+on O.OrderID=P.OrderID
+where P.Amount<O.TotalAmount
+
+--17
+Select E.Name,D.DepartmentName from Employees E
+Join Departments D
+on E.DepartmentID=D.DepartmentID
+
+--18
+Select P.ProductName,C.CategoryName from Products P
+join Categories C
+on P.Category=C.CategoryID
+where C.CategoryName in ('Electronics','Furniture');
+
+--19
+Select*from Customers1 C
+Join Sales S
+on C.CustomerID=S.CustomerID
+where  C.Country='USA'
+
+--20
+Select O.OrderID,
+    C.FirstName,
+	C.LastName,
+    C.Country,
+    O.TotalAmount from Customers1 C
+join Orders O
+on C.CustomerID=O.CustomerID
+where C.Country='Germany' and O.TotalAmount>100
+
+--21
+Select 
+E1.Name as Employee1,
+E2.Name as Employee2,
+E1.DepartmentID as Department1,
+E2.DepartmentID as Department2
+from Employees E1
+join Employees E2 on E1.EmployeeID < E2.EmployeeID
+where E1.DepartmentID <> E2.DepartmentID;
+
+--22
+Select P.PaymentID,P.OrderID,P.Amount,(O.Quantity*Pro.Price) as ExpectedAmount from Payments P
+join Orders O
+on P.OrderID=O.OrderID
+join Products Pro
+on O.ProductID=Pro.ProductID
+where P.Amount<>(O.Quantity*Pro.Price);
