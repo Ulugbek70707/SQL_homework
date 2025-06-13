ce7652dec05880e73279bb421618d849e8bdd472
Ulug'bek Xudoyberdiyev <ulugbek.khudoyberdiyev@gmail.com>
2025-06-13 18:14:27 +0500

    Update lesson10.sql

--- lesson-10/homework/lesson10.sql
+++ lesson-10/homework/lesson10.sql
@@ -60,3 +60,70 @@ join Departments D
 on E.DepartmentID=D.DepartmentID
 group by D.DepartmentName
 having count(E.EmployeeID)>5
+
+--11
+Select P.ProductID, P.ProductName from Products P
+left join Sales S
+on P.ProductID=S.ProductID
+where S.ProductID is null
+
+--12
+Select C.FirstName,C.LastName, Count(O.OrderID) as Totalorders from Customers C
+inner join Orders O
+on C.CustomerID=O.CustomerID
+group by C.FirstName, C.LastName
+order by Totalorders desc;
+
+--13
+Select E.Name as EmployeeName, D.DepartmentName from Employees E
+inner join Departments D
+on E.DepartmentID=D.DepartmentID
+
+--14
+Select E1.Name as Emp1, E2.Name as Emp2, E1.ManagerID from Employees E1
+join Employees E2
+on E1.ManagerID=E2.ManagerID
+and E1.EmployeeID<E2.EmployeeID
+where E1.ManagerID is not null
+order by E1.ManagerID
+
+--15
+Select C.FirstName, C.LastName, O.OrderID, O.OrderDate from Orders O
+join Customers C
+on C.CustomerID=O.CustomerID
+Where year(O.OrderDate)=2022;
+
+--16
+Select E.Name, E.Salary, D.DepartmentName from Employees E
+join Departments D
+on E.DepartmentID=D.DepartmentID
+where D.DepartmentName='Sales' and E.Salary>=60000
+
+--17
+Select O.OrderID, O.OrderDate, P.PaymentDate, P.Amount from Orders O
+inner join Payments P
+on O.OrderID=P.OrderID
+
+--18
+Select P.ProductID, P.ProductName from Products P
+left join Orders O
+on P.ProductID=O.ProductID
+where O.ProductID is null
+
+--19
+SELECT 
+    E.Name AS EmployeeName,
+    E.Salary
+FROM 
+    Employees E
+INNER JOIN (
+    SELECT 
+        DepartmentID, 
+        AVG(Salary) AS AvgSalary
+    FROM 
+        Employees
+    GROUP BY 
+        DepartmentID
+) AS DAvg ON E.DepartmentID = DAvg.DepartmentID
+WHERE 
+    E.Salary > DAvg.AvgSalary;
